770b099569975b513cf755ee6c0d76f8641c7780
tq303 <tq303@loc.org>
2026-05-22 12:02:59 +0100

    feat: add cobra cli lib

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/spf13/cobra"
 	"github.com/tq303/ddx/internal/file"
 )
 
-func main() {
-	lines, err := file.Read("./test.txt")
-	if err != nil {
-		return
-	}
+var rootCmd = &cobra.Command{
+	Use:   "ddx [file]",
+	Short: "CLI file parser",
+	Args: func(_ *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("file path is required, usage: ddx [file]")
+		}
+		return nil
+	},
+	Run: func(_ *cobra.Command, args []string) {
+		lines, err := file.Read(args[0])
+		if err != nil {
+			return
+		}
 
-	file.Filter(lines)
+		file.Filter(lines)
+	},
+}
+
+func main() {
+	rootCmd.Execute()
 }
